ae7e68b3fca3d3fdcc3a2539cd3aeb47da2fe07e
irtazaakram <irtaza.akram@arbisoft.com>
2023-06-06 10:59:17 +0500

    Fix linting and missing interpreter error

--- mock_django/__init__.py
+++ mock_django/__init__.py
@@ -6,7 +6,7 @@ mock_django
 :license: Apache License 2.0, see LICENSE for more details.
 """
 
-from .http import *
-from .managers import *
-from .models import *
-from .signals import *
+from .http import * # noqa: F401
+from .managers import * # noqa: F401
+from .models import * # noqa: F401
+from .signals import * # noqa: F401
